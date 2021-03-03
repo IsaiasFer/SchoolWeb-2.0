7466ee85d30e56775b801132392286ec6042e517
EMI <emiliano.2.farias@hotmail.com>
2021-03-03 09:32:56 -0300

    Contents buttons in section academic at developing. Academic section V1.4

--- assets/scripts/interactivity.js
+++ assets/scripts/interactivity.js
@@ -1,7 +1,7 @@
 let openTitle = document.getElementById('openTitle')
 let titleContent = document.getElementById('titleContent')
 
-let openStudyPlan = document.getElementById('openStudyPlan') 
+let openStudyPlan = document.getElementById('openStudyPlan')
 let studyPlanContent = document.getElementById('studyPlanContent')
 
 let openStudyProgram = document.getElementById('openStudyProgram')
@@ -10,33 +10,33 @@ let studyProgramContent = document.getElementById('studyProgramContent')
 let openTeachersInfo = document.getElementById('openTeachersInfo')
 let infoTeachersContent = document.getElementById('infoTeachersContent')
 
-let x = [titleContent, studyPlanContent, studyProgramContent, infoTeachersContent]
+let contents = [titleContent, studyPlanContent, studyProgramContent, infoTeachersContent]
 
 
 openTitle.addEventListener('click', function(){
     titleContent.style.display = 'flex'
-    x[1].style.display = 'none'
-    x[2].style.display = 'none'
-    x[3].style.display = 'none'
+    contents[1].style.display = 'none'
+    contents[2].style.display = 'none'
+    contents[3].style.display = 'none'
 })
 
 openStudyPlan.addEventListener('click', function(){
     studyPlanContent.style.display = 'flex'
-    x[0].style.display = 'none'
-    x[2].style.display = 'none'
-    x[3].style.display = 'none'
+    contents[0].style.display = 'none'
+    contents[2].style.display = 'none'
+    contents[3].style.display = 'none'
 })
 
 openStudyProgram.addEventListener('click', function(){
-    studyProgramContent.style.display = 'flex'
-    x[3].style.display = 'none'
-    x[1].style.display = 'none'
-    x[0].style.display = 'none'
+    studyProgramContent.style.display = 'inline-block'
+    contents[3].style.display = 'none'
+    contents[1].style.display = 'none'
+    contents[0].style.display = 'none'
 })
 
 openTeachersInfo.addEventListener('click', function(){
     infoTeachersContent.style.display = 'flex'
-    x[2].style.display = 'none'
-    x[1].style.display = 'none'
-    x[0].style.display = 'none'
+    contents[2].style.display = 'none'
+    contents[1].style.display = 'none'
+    contents[0].style.display = 'none'
 })
